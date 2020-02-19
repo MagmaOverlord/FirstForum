0b876fc1dd567c453efccf853108da16f6321c3f
audreyandoy <audreylandoy@gmail.com>
2020-02-18 19:44:53 -0800

    Added possibility to post - need to write code do display

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import request
 
 app = flask.Flask(__name__)
 
+generalP = []
+gamesP = []
+tvP = []
+booksP = []
+
 @app.route('/')
 def home():
   return flask.render_template('index.html')
@@ -10,6 +15,22 @@ def home():
 @app.route('/about')
 def about():
     return flask.render_template('about.html')
+
+@app.route('/books')
+def books():
+    return flask.render_template('books.html')
+
+@app.route('/games')
+def games():
+    return flask.render_template('games.html')
+
+@app.route('/general')
+def general():
+    return flask.render_template('general.html')
+
+@app.route('/tv')
+def tv():
+    return flask.render_template('tv.html')
 
 @app.route('/post')
 def post():
